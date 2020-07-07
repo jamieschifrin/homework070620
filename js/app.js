9a159f0e98dae5c162fbe850860b63c0dac62589
Jamie Schrifrin <jamieschifrin@hotmail.com>
2020-07-06 20:19:19 -0400

    first commit

---
+++ js/app.js
@@ -0,0 +1,40 @@
+console.log('isitworking')
+
+var first= 1;
+var second= 2;
+var third= 3;
+var fourth= 4;
+var fifth= 5;
+var sixith= 6;
+var seventh= 7;
+var eighth= 8;
+var ninth= 9;
+var tenth= 10;
+
+var addinga= first + second;
+var addingb= third + fourth;
+
+console.log(addinga)
+console.log(addingb)
+
+var multa= first * second;
+var multb= third * fourth;
+
+console.log(multa)
+console.log(multb)
+
+var divisiona= first / second;
+var divisionb= third / fourth;
+
+console.log(divisiona)
+console.log(divisionb)
+
+first++;
+console.log(first)
+
+third= third + 1;
+console.log(third)
+
+
+
+
